eb3092c30cc68d0eb47c27484c53d2095bfc21dc
McBaque <mcbaque01@gmail.com>
2024-05-22 17:15:55 +0800

    Commiting initial working SMTP Nodemailer

--- Backend/app/index.js
+++ Backend/app/index.js
@@ -1,5 +1,6 @@
 import express from "express"; // nodejs framework
 import dotenv from "dotenv";
+import nodemailer from 'nodemailer';
 const app = express();
 const port = 8080;
 app.use(express.json());
@@ -7,8 +8,29 @@ dotenv.config();
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
     console.log(process.env.TEST_ENV);
+    console.log(process.env.GMAIL_EMAIL);
+    console.log(process.env.GMAIL_PASSWORD);
 });
 // Error handling for server setup
 app.on('error', (err) => {
     console.error('Server error:', err);
 });
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASSWORD,
+    },
+});
+const EmailSender = async () => {
+    const info = await transporter.sendMail({
+        from: `"TEST GMAIL" ${process.env.GMAIL_EMAIL}`, // sender address
+        to: "mcbaque01@gmail.com", // list of receivers
+        subject: "Test Messsage!", // Subject line
+        text: "Hello worldEST?", // plain text body
+    });
+    console.log("Message sent: %s", info.messageId);
+};
+EmailSender();
