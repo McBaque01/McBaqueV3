a91901efec9ab7ac8740f749eb949b12a3b1c171
McBaque <mcbaque01@gmail.com>
2024-05-29 21:58:32 +0800

    Fixing bugs!

--- Backend/app/config/env.js
+++ Backend/app/config/env.js
@@ -1,5 +1,5 @@
 export const env = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     mongoURI: process.env.MongoDbURI || 'mongodb+srv://render-admin-user:renderadminpassword.@cluster0.npib522.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     GmailEmail: process.env.GMAIL_EMAIL || 'mcbaqueee@gmail.com',
     GmailPassword: process.env.GMAIL_PASSWORD || 'rztepqnpitwsfoul',
