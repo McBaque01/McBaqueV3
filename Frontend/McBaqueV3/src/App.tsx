68d68edcf4f5512b7672d7373751f07d0b7616ee
McBaque <mcbaque01@gmail.com>
2024-05-30 11:19:03 +0800

    Implement Helmet on app.tsx

--- Frontend/McBaqueV3/src/App.tsx
+++ Frontend/McBaqueV3/src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
 import { RouteHero } from "./assets/routes/RouteHero";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+    <Helmet>
+        <style>{`
+          /* Your critical CSS goes here */
+          body { 
+            font-family: Arial, sans-serif; 
+          }
+          h1 {
+            font-size: 3em; /* Assuming this is part of your critical styles */
+          }
+          /* Add other critical styles as needed */
+        `}</style>
+      </Helmet>
       <RouteHero />
     </>
   );
