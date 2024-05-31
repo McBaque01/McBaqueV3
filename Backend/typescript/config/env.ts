1803000d71d08dd044fd58c8b7368fee94c54b38
McBaque <mcbaque01@gmail.com>
2024-05-31 17:27:50 +0800

    Commiting env.js

---
+++ Backend/typescript/config/env.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const env = {
+    Port: process.env.PORT || "PORT_NUMBER",
+    Host: process.env.HOST || "LOCAL_HOST",
+    MongoURI: process.env.MongoDbURI || "MONGO_DB_URI",
+    GmailEmail: process.env.GMAIL_EMAIL || "GMAIL_EMAIL",
+    GmailPassword: process.env.GMAIL_PASSWORD || "GMAIL_PASSWORD",
+    SpotifyClient: process.env.SpotifyClientID || "SPOTIFY_CLIENT_ID",
+    SpotifySecret: process.env.SpotifySecret || "SPOTIFY_SECRET",
+  };
+
+  
