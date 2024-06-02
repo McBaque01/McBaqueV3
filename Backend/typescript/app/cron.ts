d1050b29cc035b6d66bbb3220723ce5967575e9e
McBaque <mcbaque01@gmail.com>
2024-06-02 13:38:04 +0800

    Trying vercel

--- Backend/typescript/app/cron.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export default function handler(req: Request, res: Response) {
-    // Your serverless function logic here
-    res.status(200).end('Hello Cron!');
-  }
