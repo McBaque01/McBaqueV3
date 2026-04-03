c2deb3d78948168251755d2e2b8f01eaf1b4bfb0
Mc Baque <mcbaque01@gmail.com>
2026-04-04 00:21:49 +0800

    put redirect to linkedin

--- Frontend/McBaqueV3/src/assets/pages/Maintenance.tsx
+++ Frontend/McBaqueV3/src/assets/pages/Maintenance.tsx
@@ -5,8 +5,8 @@ export const Maintenance = () => {
       <Icon icon="streamline-cyber-color:maintenance" width="200" height="200" />
       <h1 className="text-6xl font-bold text-gray-200">Site Under Maintenance</h1>
       <h1 className="text-2xl font-bold text-gray-200">I’m currently rebuilding my portfolio to serve you better.</h1>
-      <h1 className="text-2xl font-bold text-gray-200">If you’d like to connect, please email me at</h1>
-      <h1 className="text-2xl font-bold text-blue-600">mcbaque01@gmail.com / www.linkedin.com/in/mcbaque</h1>
+      <h1 className="text-2xl font-bold text-gray-200">If you’d like to connect, please email/message me at</h1>
+      <h1 className="text-2xl font-bold text-blue-600">mcbaque01@gmail.com / <a href="https://www.linkedin.com/in/mcbaque" target="_blank" rel="noopener noreferrer">www.linkedin.com/in/mcbaque</a></h1>
     </div>
   );
 }   
