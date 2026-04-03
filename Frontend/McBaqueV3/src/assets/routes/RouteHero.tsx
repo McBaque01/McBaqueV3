9a68ad138d5b625cdf08c28960fc08245a33d220
Mc Baque <mcbaque01@gmail.com>
2026-04-03 23:09:47 +0800

    Commit correct routing

--- Frontend/McBaqueV3/src/assets/routes/RouteHero.tsx
+++ Frontend/McBaqueV3/src/assets/routes/RouteHero.tsx
@@ -1,18 +1,36 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import { Portfolio } from "../pages/Portfolio";
+
 
 import { Maintenance } from "../pages/Maintenance";
+import { Portfolio } from "../pages/Portfolio";
 
 export const RouteHero = () => {
+
+    const isMaintenance = true; 
   return (
     <BrowserRouter>
       <Routes>
+
+       {isMaintenance ? (
+          <>
+            <Route path="/Maintenance" element={<Maintenance />} />
+            <Route path="*" element={<Navigate to="/Maintenance" />} />
+          </>
+        ) : (
+          <>
+             <Route path="/" element={<Navigate to="/Portfolio" />} />
+            <Route path="/Portfolio" element={<Portfolio />} />
+
+          </>
+        )}
+
+
         {/* <Route path="/" element={<Navigate to="/Portfolio" />} />
         <Route path="/Portfolio" element={<Portfolio />} /> */}
 
-        <Route path="/" element={<Navigate to="/Maintenance" />} />
-        <Route path="/Maintenance" element={<Maintenance />} />
-        
+        {/* <Route path="/" element={<Navigate to="/Maintenance" />} />
+        <Route path="/Maintenance" element={<Maintenance />} /> */}
+
       </Routes>
     </BrowserRouter>
   );
